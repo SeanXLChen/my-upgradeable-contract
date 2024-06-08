c59aa3091418ef6123a25d3091116f5e8b1b7713
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-06-08 07:36:18 -0700

    Feat: deploy proxy contract + v1 implementation contract

--- ignition/modules/VendingMachineV1.js
+++
@@ -1,7 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("VendingMachineModule", (m) => {
-    const deployer = m.getAccount(0);
-    const counter = m.contract("VendingMachineV1", [], { from: deployer });
-    return { counter };
-});
